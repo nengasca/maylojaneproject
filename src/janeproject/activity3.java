02d1385ef899dd326e95820af202bfc6c1b22471
nengasca <maylojanen@gmail.com>
2025-07-29 14:03:02 +0800

    output

---
+++ src/janeproject/activity3.java
@@ -0,0 +1,29 @@
+
+package janeproject;
+
+import java.util.Scanner;
+
+
+public class activity3 {
+    
+      public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+
+        if (number > 0) {
+            System.out.println(number + " is a positive number");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number");
+        } else {
+            System.out.println("Inputted number Zero");
+        }
+
+        sc.close();
+
+    }
+    
+}
+    
+
